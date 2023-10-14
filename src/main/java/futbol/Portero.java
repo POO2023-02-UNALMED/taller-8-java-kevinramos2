8ee6fccc2b565fd621e56df207158b136e83949b
kevinramos2 <keramosl@unal.edu.co>
2023-10-14 18:52:09 -0500

    Create Portero.java

---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,35 @@
+package futbol;
+
+public class Portero extends Futbolista {
+	//atributos
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	//constructor
+	public Portero(String nombre, String posicion, int edad, short golesRecibidos, byte dorsal) {
+		super(nombre,"portero",edad);
+		this.dorsal = dorsal;
+		this.golesRecibidos = golesRecibidos;
+	}
+	
+	//toString
+	@Override
+	public String toString(){
+	   return super.toString() + " con el dorsal " + dorsal + ". Le han marcado " + golesRecibidos;
+	}
+	@Override
+	public int compareTo(Futbolista o) {
+		var otroPortero = (Portero) o;
+	    return otroPortero.getGolesRecibidos()-golesRecibidos;
+	}
+
+	private short getGolesRecibidos() {
+		return this.golesRecibidos;
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		return true;
+	}
+
+}
